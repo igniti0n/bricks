c4b20c957888ebe3e9018db7cb903983ad189977
istajcer <ivan.stajcer@q.agency>
2023-07-18 18:20:07 +0200

    updated page brick

--- page/__brick__/lib/presentation/{{name.snakeCase()}}_page/{{name.snakeCase()}}.dart
+++ page/__brick__/lib/presentation/{{name.snakeCase()}}_page/{{name.snakeCase()}}.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'components/{{name.snakeCase()}}_page_app_bar.dart';
+import 'components/{{name.snakeCase()}}_app_bar.dart';
 import 'components/{{name.snakeCase()}}_page_body.dart';
 {{#description}}
 /*
 {{description}}
  */{{/description}}
-class {{name.pascalCase()}}Page extends ConsumerWidget {
-  const {{name.pascalCase()}}Page({super.key});
+class {{name.pascalCase()}} extends ConsumerWidget {
+  const {{name.pascalCase()}}({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return const Scaffold(
-      appBar: {{name.pascalCase()}}PageAppBar(),
-      body: {{name.pascalCase()}}PageBody(),
+      appBar: {{name.pascalCase()}}AppBar(),
+      body: {{name.pascalCase()}}Body(),
     );
   }
 }
